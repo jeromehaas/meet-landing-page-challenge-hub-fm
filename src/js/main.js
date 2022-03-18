b9c996d23f486e062cdce12a5c6f0cd61bdc431c
Jerome Haas <hello@jeromehaas.dev>
2022-03-18 17:22:31 +0100

    Refactor: prevent script from playing animation for frontendmentor screenshot

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,7 @@ class Animation {
       if (this.willPlay) {
 				console.log(this.elements.body);
 				this.elements.body.classList.remove('body--hidden');
-        this.playTimeline();
+        // this.playTimeline();
       } else {
 				this.elements.body.classList.remove('body--hidden');
 			};
@@ -98,7 +98,7 @@ class Animation {
   checkAnimationCookie() {
     const animationCookie = Cookies.get('last-animation-render');
     if (!animationCookie) {
-      // this.willPlay = true;
+      this.willPlay = true;
       const today = moment().format();
       Cookies.set('last-animation-render', today, {
         expires: 1
